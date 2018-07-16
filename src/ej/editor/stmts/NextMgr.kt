2c6f5cfb139d988524a9f8c98086b229775df3b2
aimozg <aimozg@ya.ru>
2018-07-17 00:08:14 +0600

    scene chooser in <next>

--- src/ej/editor/stmts/NextMgr.kt
+++ src/ej/editor/stmts/NextMgr.kt
@@ -1,15 +1,23 @@
 package ej.editor.stmts
 
 import ej.editor.Styles
+import ej.editor.expr.valueLink
 import ej.editor.views.StatementTree
+import ej.mod.Builtins
 import ej.mod.XComplexStatement
 import ej.mod.XsNext
+import ej.mod.acceptsMenu
+import javafx.scene.text.TextFlow
 import tornadofx.*
 
 object NextMgr : StatementManager<XsNext>() {
-	override fun editorBody(stmt: XsNext, rootStmt: XComplexStatement) = defaultEditorBody {
-		label("Proceed to scene")
-		textfield(stmt.refProperty)
+	override fun editorBody(stmt: XsNext, rootStmt: XComplexStatement) = defaultEditorBody(TextFlow()) {
+		text("Proceed to scene ")
+		valueLink("Scene",
+		          stmt.refProperty,
+		          SceneChooser(rootStmt,
+		                       controller.mod ?: return@defaultEditorBody,
+		                       Builtins.scenes) { it.acceptsMenu })
 	}
 
 	override fun treeGraphic(stmt: XsNext, tree: StatementTree) = simpleTreeLabel(
